5b313eb666ae2cce5c24a23440f2c13d885b98f9
ABT\furmanm <Marschall_Furman@abtassoc.com>
2023-12-11 14:07:08 -0500

    add select input

--- app.R
+++ app.R
@@ -5,11 +5,16 @@ library(EJAMejscreenapi)
 
 ui <- fluidPage(
   h3('Test header pt. 2'),
-  h5('test subheader')
+  h5('test subheader'),
+  
+  selectInput('test-sel', label = 'Choose an option', choices = c('A','B','C'))
 )
 
 server <- function(input, output, session) {
-  
+  observeEvent(input$test_sel, {
+    cat(paste0('this is working! you chose ', input$test_sel))
+    
+  })
 }
 
 shinyApp(ui, server)
