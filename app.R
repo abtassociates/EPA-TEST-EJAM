8a89deb5cf56c5cd5994dfa52f77e5f400c6ecac
ABT\furmanm <Marschall_Furman@abtassoc.com>
2023-12-14 13:43:41 -0500

    attempt to read data from pins board

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 
+source('global.R')
 
 ui <- fluidPage(
   h3('Test header pt. 2'),
@@ -7,13 +8,25 @@ ui <- fluidPage(
   
   h5('New code showing up!'),
   
-  selectInput('test_sel', label = 'Choose an option', choices = c('A','B','C'))
+  selectInput('test_sel', label = 'Choose an option', choices = c('A','B','C')),
+  verbatimTextOutput('test_sel_text'),
+  
+  tableOutput('pins_table')
 )
 
 server <- function(input, output, session) {
   observeEvent(input$test_sel, {
     cat(paste0('this is working! you chose ', input$test_sel))
     
+  })
+  
+  output$test_sel_text <- renderText({
+    paste0('this dropdown is working! you chose ', input$test_sel)
+  })
+  
+  output$pins_table <- renderTable({
+    dataload_from_pins(varnames = 'bgpts')
+    head(bgpts)
   })
 }
 
